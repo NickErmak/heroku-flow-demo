6c203c040ac7eb9eaf5486f56aef2f2246a3f345
NickErmak <30970151+NickErmak@users.noreply.github.com>
2022-06-29 16:10:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-var _ = require("lodash");
-var router = express.Router();
 require('dotenv').config()
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,55 +8,20 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
 
-router.get('/', function(request, response) {
-    var env = process.env.APP_ENV;
-    if (env == 'staging') {
-        var envName = 'staging'
-    } else if (env == 'production') {
-        var envName = 'production'
-    } else {
-        var envName = 'review app'
-    }
-    response.render('index.html', {
-        env: envName
-    });
+app.get('/', function(request, response) {
+  var env = process.env.APP_ENV;
+  if (env == 'staging') {
+    var envName = 'staging'
+  } else if (env == 'production') {
+    var envName = 'production'
+  } else {
+    var envName = 'review app'
+  }
+  response.render('index.html', { env: envName});
 });
 
-router.post("/new_contact", function(req, res) {
-
-    // get the obm as an object
-    var message = unwrapMessage(req.body);
-    if (!_.isEmpty(message)) {
-
-        // some something #awesome with message
-        console.log(message);
-
-        // return a ‘true’ Ack to Salesforce
-        res.send(
-            'soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:out="http://soap.sforce.com/2005/09/outbound">true</soapenv:Envelope'
-        );
-    } else {
-        // return a ‘false’ Ack to Salesforce
-        res.send(
-            'soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:out="http://soap.sforce.com/2005/09/outbound">false</soapenv:Envelope'
-        );
-    }
+app.listen(app.get('port'), function() {
+  console.log("Node app running at localhost:" + app.get('port'));
 });
 
-// unwrap the xml and return object
-unwrapMessage = function(obj) {
-    try {
-        var orgId = obj['soapenv:envelope']['soapenv:body'][0].notifications[0].organizationid[0];
-        var contactId = obj['soapenv:envelope']['soapenv:body'][0].notifications[0].notification[0].sobject[0]['sf:id'][0];
-        var mobilePhone = obj['soapenv:envelope']['soapenv:body'][0].notifications[0].notification[0].sobject[0]['sf:mobilephone'][0];
-        return {
-            orgId: orgId,
-            contactId: contactId,
-            mobilePhone: mobilePhone
-        };
-    } catch (e) {
-        console.log('Could not parse OBM XML', e);
-        return {};
-    }
-};
-module.exports = router;
+module.exports = app
